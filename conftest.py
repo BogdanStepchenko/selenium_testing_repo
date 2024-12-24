29d88a4084a340cc372a23a88f56df4aea475b67
b_stepchenko <b_stepchenko@wargaming.net>
2024-12-24 16:46:55 +0200

    headed_web_driver

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from pages.sign_in_page import SignInPage
 from pages.account_page import AccountPage
 from pages.collections_eco_friendly_page import EcoFriendly
@@ -9,7 +12,15 @@ from pages.women_sale_page import WomenSale
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
 
